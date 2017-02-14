f3eb54ed53141f1a1f4faf34d1475f54be5f4a45
redtoorange <redtoorange@gmail.com>
2017-02-14 15:43:44 -0500

    Refactoring

--- src/control/DictionaryLoaderController.java
+++ src/control/DictionaryLoaderController.java
@@ -36,11 +36,18 @@ public class DictionaryLoaderController {
 	private FileChooser dictionaryChooser;
 	private File selectedFile;
 
+	/**
+	 *	Hook this controller back to the application controller to allow the callback for when the generate button is clicked
+	 * @param loader The main ApplicationController that handles application state and the controllers/views
+	 */
 	public void init( ApplicationController loader ){
 		launchLoader = loader;
 		initChooser();
 	}
 
+	/**
+	 *	Initialize the file chooser and set it's filter and title.
+	 */
 	private void initChooser() {
 		dictionaryChooser = new FileChooser( );
 		dictionaryChooser.setTitle( FILE_CHOOSER_TITLE );
@@ -49,6 +56,11 @@ public class DictionaryLoaderController {
 		dictionaryChooser.getExtensionFilters().add( filter );
 	}
 
+	/**
+	 *	Event handler method for when the generate button is clicked.  It is disabled until a Valid file
+	 *	is selected or typed in and processed.
+	 * @param event Not Used
+	 */
 	@FXML public void generateButtonClicked( ActionEvent event){
 		try{
 			DictionaryFile dictionaryFile = new DictionaryFile( selectedFile );
@@ -56,25 +68,33 @@ public class DictionaryLoaderController {
 		}
 		catch( IncompleteWordException iwe){
 			displayInvalidAlert();
-			disableGenerateButton();
+			generateButton.setDisable( true );
 		}
 		catch(FileNotFoundException e){
 			displayInvalidAlert();
-			disableGenerateButton();
+			generateButton.setDisable( true );
 		}
 	}
 
+	/**
+	 *	The FXML injected event handler for the file chooser button.
+	 * @param event	Not Used
+	 */
 	@FXML public void fileChooserButtonClicked( ActionEvent event){
 		launchFileChooser( );
 	}
 
+	/**
+	 *	Launch the file chooser to select a dictionary file in the form of a .txt file.  If the user selects
+	 *	a file, it is sent to be processed for validity (future proof for other file formats).
+	 */
 	private void launchFileChooser() {
 		File chosenFile = null;
 		try{
 			chosenFile = dictionaryChooser.showOpenDialog( null );
 		}
 		catch ( NullPointerException npe){
-			displayInvalidAlert( );
+			displayInvalidAlert( );	//Handles a bug where the user selects an icon and tries to load it
 		}
 
 		if( chosenFile != null ) {
@@ -82,34 +102,43 @@ public class DictionaryLoaderController {
 		}
 	}
 
+	/**
+	 * Injected method called by the FXML file when the text is entered into the TextField and the user presses
+	 * enter.  The file is checked for existence and then processed for validity.
+	 * @param event	Not used.
+	 */
 	@FXML public void filePathTextEntered( ActionEvent event ){
 		File enteredFile = new File( filePathText.getText() );
 
-		if(enteredFile.exists())
+		if( enteredFile.exists() )
 			processFile( enteredFile );
+		else{
+			displayInvalidAlert();
+		}
 	}
 
+	/**
+	 *	Check that the file that was selected/type is actually valid.  If it is valid, enable the generate button.
+	 *	This is just a simple extension check for the file, it does not parse the contents.  The contents are parsed
+	 * when the file is loaded by the dictionary loader.  If the contents are invalid, then a warning is displayed again.
+	 *  @param file	The selected or typed file to check.
+	 */
 	private void processFile( File file ) {
 		if( FileValidator.valid( file ) ) {
 			selectedFile = file;
 			filePathText.setText( selectedFile.getAbsolutePath( ) );
-			enableGenerateButton( );
+			generateButton.setDisable( false );
 		}
 		else {
 			selectedFile = null;
-			disableGenerateButton( );
+			generateButton.setDisable( true );
 			displayInvalidAlert( );
 		}
 	}
 
-	private void enableGenerateButton(){
-		generateButton.setDisable( false );
-	}
-
-	private void disableGenerateButton(){
-		generateButton.setDisable( true );
-	}
-
+	/**
+	 *	Helper function to display a warning message when there is an error loading the file.
+	 */
 	private void displayInvalidAlert( ){
 		Alert alert = new Alert( Alert.AlertType.ERROR, INVALID_FILE_MSG );
 		alert.showAndWait();
--- src/control/PuzzleImageController.java
+++ src/control/PuzzleImageController.java
@@ -19,8 +19,8 @@ public class PuzzleImageController {
 	private static final double PAPER_HEIGHT = 11;
 	private static final int PIXELS_PER_INCH = 150;
 
-	private static final int IMAGE_WIDTH = (int)(PAPER_WIDTH * PIXELS_PER_INCH);
-	private static final int IMAGE_HEIGHT = (int)(PAPER_HEIGHT * PIXELS_PER_INCH);
+	private static final int IMAGE_WIDTH = ( int ) ( PAPER_WIDTH * PIXELS_PER_INCH );
+	private static final int IMAGE_HEIGHT = ( int ) ( PAPER_HEIGHT * PIXELS_PER_INCH );
 
 	private int width = 0;
 	private int height = 0;
@@ -34,11 +34,14 @@ public class PuzzleImageController {
 	private Graphics2D blankPuzzleWriter = null;
 
 	/**
+	 * Factory Method to create a new puzzle image with the given buffer from the given grid.  The words and
+	 * definitions will be pulled from the Words array list inside the grid.
 	 *
-	 * @param grid
-	 * @param buffer
+	 * @param grid   The Grid to use to create the images
+	 * @param buffer The buffer is the number of pixels to pad the edge of the image by and the amount of spacing for
+	 *               the definitions.
 	 */
-	public PuzzleImage createPuzzleImage( Grid grid, int buffer ){
+	public PuzzleImage createPuzzleImage( Grid grid, int buffer ) {
 		setDimensions( grid, buffer );
 
 		BufferedImage blankImage = new BufferedImage( IMAGE_WIDTH, IMAGE_HEIGHT, BufferedImage.TYPE_INT_ARGB );
@@ -50,10 +53,10 @@ public class PuzzleImageController {
 		blankPuzzleWriter = createGraphics2dWriter( blankImage );
 
 
-		buildGrid( grid.getLetterGrid() );
+		buildGrid( grid.getLetterGrid( ) );
 		writeWordNumbersOnGrid( grid );
 
-		puzzleImage.storeTempFiles();
+		puzzleImage.storeTempFiles( );
 
 		answerKeyWriter = null;
 		blankPuzzleWriter = null;
@@ -61,26 +64,28 @@ public class PuzzleImageController {
 	}
 
 	/**
-	 *	Prepare the state of the Object to create the correctly sized Image based on the input grid.
-	 * @param grid The Grid that the image will be based on.
+	 * Prepare the state of the Object to create the correctly sized Image based on the input grid.
+	 *
+	 * @param grid   The Grid that the image will be based on.
 	 * @param buffer How much of a buffer that should be used when building the image.
 	 */
 	private void setDimensions( Grid grid, int buffer ) {
 		this.buffer = buffer;
-		width = (IMAGE_WIDTH - buffer ) / grid.getWidth();
-		height = ( (int)(IMAGE_HEIGHT * .65) - buffer) / grid.getHeight();
+		width = ( IMAGE_WIDTH - buffer ) / grid.getWidth( );
+		height = ( ( int ) ( IMAGE_HEIGHT * .65 ) - buffer ) / grid.getHeight( );
 
 		cellSize = Math.min( width, height );
 
-		yOffset = buffer/2;
-		xOffset = (IMAGE_WIDTH - ( cellSize * grid.getWidth( )) ) / 2;
+		yOffset = buffer / 2;
+		xOffset = ( IMAGE_WIDTH - ( cellSize * grid.getWidth( ) ) ) / 2;
 		fontSize = cellSize;
 	}
 
 	/**
 	 * Write the word numbers on the Grid and then Write the corresponding definitions under the grid.
-	 * @param grid The Grid object's WordList and the WordPlacements of those words will be used to correctly
-	 *             write the definitions.
+	 *
+	 * @param grid The Grid object's WordList and the WordPlacements of those words will be used to correctly write the
+	 *             definitions.
 	 */
 	private void writeWordNumbersOnGrid( Grid grid ) {
 		resetGraphics2DWriters( );
@@ -89,16 +94,16 @@ public class PuzzleImageController {
 		String horizontals = "Across: \n";
 		int wordNumber = 1;
 
-		for( Word w : grid.getWordsOnGrid()){
-			Vector2 pos = w.getWordPlacement().getStartPosition();
+		for ( Word w : grid.getWordsOnGrid( ) ) {
+			Vector2 pos = w.getWordPlacement( ).getStartPosition( );
 
-			answerKeyWriter.drawString( "" + wordNumber,  pos.getX() * cellSize + xOffset + (int)(cellSize*.1), pos.getY() * cellSize + yOffset + (int)(cellSize * 0.6)  );
-			blankPuzzleWriter.drawString( "" + wordNumber,  pos.getX() * cellSize + xOffset + (int)(cellSize*.1), pos.getY() * cellSize + yOffset + (int)(cellSize * 0.6)  );
+			answerKeyWriter.drawString( "" + wordNumber, pos.getX( ) * cellSize + xOffset + ( int ) ( cellSize * .1 ), pos.getY( ) * cellSize + yOffset + ( int ) ( cellSize * 0.6 ) );
+			blankPuzzleWriter.drawString( "" + wordNumber, pos.getX( ) * cellSize + xOffset + ( int ) ( cellSize * .1 ), pos.getY( ) * cellSize + yOffset + ( int ) ( cellSize * 0.6 ) );
 
-			if(w.getWordPlacement().getOrientation() == Orientation.HORIZONTAL)
-				horizontals += "\t" + wordNumber + " : " + w.getDefinitionString() + "\n";
+			if ( w.getWordPlacement( ).getOrientation( ) == Orientation.HORIZONTAL )
+				horizontals += "\t" + wordNumber + " : " + w.getDefinitionString( ) + "\n";
 			else
-				vertical += "\t" + wordNumber + " : " + w.getDefinitionString() + "\n";
+				vertical += "\t" + wordNumber + " : " + w.getDefinitionString( ) + "\n";
 
 			wordNumber++;
 		}
@@ -107,20 +112,22 @@ public class PuzzleImageController {
 	}
 
 	/**
+	 * Write the definitions along with their number sorted into the "Across" and "Down" sections
 	 *
-	 * @param grid
-	 * @param vertical
-	 * @param horizontals
-	 * @param wordNumber
+	 * @param grid        The grid where the words come from.
+	 * @param vertical    A string containing all of the definions for the "Down" words
+	 * @param horizontals A string containing all the definitions for the "Across" words
+	 * @param wordNumber  The total number of words.  Used to calculate a font size to fit all the words into the given
+	 *                    area.
 	 */
 	private void writeDefinitionsUnderGrid( Grid grid, String vertical, String horizontals, int wordNumber ) {
-		fontSize = (int)(IMAGE_HEIGHT * .35) / (wordNumber + 4);
+		fontSize = ( int ) ( IMAGE_HEIGHT * .35 ) / ( wordNumber + 4 );
 
-		answerKeyWriter.setFont( new Font("TimesRoman", Font.PLAIN, fontSize ) );
-		blankPuzzleWriter.setFont( new Font("TimesRoman", Font.PLAIN, fontSize ) );
+		answerKeyWriter.setFont( new Font( "TimesRoman", Font.PLAIN, fontSize ) );
+		blankPuzzleWriter.setFont( new Font( "TimesRoman", Font.PLAIN, fontSize ) );
 
 		int row = 1;
-		int initialY = grid.getHeight() * cellSize + yOffset + fontSize;
+		int initialY = grid.getHeight( ) * cellSize + yOffset + fontSize;
 
 		row = writeStringOfDefinitions( vertical, row, initialY );
 		row++;
@@ -128,23 +135,25 @@ public class PuzzleImageController {
 	}
 
 	/**
-	 *	Reset the two Graphics2D objects to write the definitions under the grid.
+	 * Reset the two Graphics2D objects to write the words numbers on the grid.
 	 */
 	private void resetGraphics2DWriters( ) {
-		answerKeyWriter.setFont( new Font("TimesRoman", Font.BOLD, (int)(cellSize * .75) ) );
-		blankPuzzleWriter.setFont( new Font("TimesRoman", Font.BOLD, (int)(cellSize * .75) ) );
+		answerKeyWriter.setFont( new Font( "TimesRoman", Font.BOLD, ( int ) ( cellSize * .75 ) ) );
+		blankPuzzleWriter.setFont( new Font( "TimesRoman", Font.BOLD, ( int ) ( cellSize * .75 ) ) );
 		answerKeyWriter.setColor( Color.BLACK );
 		blankPuzzleWriter.setColor( Color.BLACK );
 	}
 
 
-
 	/**
+	 * Writes a series of definitions on the board under the grid.  Starting at initial Y, it counts down
+	 * "row" rows based on the font size, and begins writing there.
 	 *
-	 * @param string
-	 * @param row
-	 * @param initialY
-	 * @return
+	 * @param string   All the definitions that need to be written under the grid.
+	 * @param row      the row on the page from the initial Y that this method begins at
+	 * @param initialY The initial Y for the definition section
+	 *
+	 * @return the row on the page from the initial Y that this method finished at
 	 */
 	private int writeStringOfDefinitions( String string, int row, int initialY ) {
 		boolean heading = true;
@@ -152,15 +161,15 @@ public class PuzzleImageController {
 		String def;
 		int x, y;
 
-		while(stringScanner.hasNext()){
-			def = stringScanner.next();
-			x = buffer + (heading ? 0 : fontSize);
-			y = initialY + (row * fontSize );
+		while ( stringScanner.hasNext( ) ) {
+			def = stringScanner.next( );
+			x = buffer + ( heading ? 0 : fontSize );
+			y = initialY + ( row * fontSize );
 
 			answerKeyWriter.drawString( def, x, y );
 			blankPuzzleWriter.drawString( def, x, y );
 
-			if(heading)
+			if ( heading )
 				heading = false;
 
 			row++;
@@ -169,20 +178,20 @@ public class PuzzleImageController {
 	}
 
 	/**
-	 *	Build the grid itself out of a 2D array of GridCell's.  Both BufferedImages will be written to.  One with just
-	 *	the boxes, and the other with the boxes and letters.
+	 * Build the grid itself out of a 2D array of GridCell's.  Both BufferedImages will be written to.  One with just
+	 * the boxes, and the other with the boxes and letters.
+	 *
 	 * @param letterGrid The 2D array of GridCell's that this PuzzleImage is being built from.
 	 */
 	private void buildGrid( GridCell[][] letterGrid ) {
-		for(int x = 0; x < letterGrid.length; x++){
-			for(int y = 0; y < letterGrid[x].length; y++){
-				if( letterGrid[x][y] != null && letterGrid[x][y].getCharacter(  ) != 0){
+		for ( int x = 0; x < letterGrid.length; x++ ) {
+			for ( int y = 0; y < letterGrid[ x ].length; y++ ) {
+				if ( letterGrid[ x ][ y ] != null && letterGrid[ x ][ y ].getCharacter( ) != 0 ) {
 					answerKeyWriter.setColor( Color.red );
-					answerKeyWriter.drawString( "" + letterGrid[x][y].getCharacter(),  x * cellSize + xOffset + (cellSize/4),y * cellSize + yOffset + (int)(cellSize * 0.85)  );
+					answerKeyWriter.drawString( "" + letterGrid[ x ][ y ].getCharacter( ), x * cellSize + xOffset + ( cellSize / 4 ), y * cellSize + yOffset + ( int ) ( cellSize * 0.85 ) );
 
-					drawHollowRect(x, y, Color.black);
-				}
-				else if( letterGrid[x][y] == null || letterGrid[x][y].getCharacter(  ) == 0){
+					drawHollowRect( x, y, Color.black );
+				} else if ( letterGrid[ x ][ y ] == null || letterGrid[ x ][ y ].getCharacter( ) == 0 ) {
 					drawFilledRect( x, y, Color.black );
 				}
 			}
@@ -190,48 +199,54 @@ public class PuzzleImageController {
 	}
 
 	/**
+	 * Draw a filled rect on the blank and answered images.
 	 *
-	 * @param x
-	 * @param y
-	 * @param color
+	 * @param x     The x of the grid.
+	 * @param y     The y of the Grid.
+	 * @param color What color to use.
 	 */
+	//TODO: Refactor this
 	private void drawFilledRect( int x, int y, Color color ) {
-		answerKeyWriter.setColor(color);
-		answerKeyWriter.fillRect( 	x * cellSize + xOffset,
+		answerKeyWriter.setColor( color );
+		answerKeyWriter.fillRect( x * cellSize + xOffset,
 				y * cellSize + yOffset,
 				cellSize, cellSize );
 
 		blankPuzzleWriter.setColor( color );
-		blankPuzzleWriter.fillRect( 	x * cellSize + xOffset,
+		blankPuzzleWriter.fillRect( x * cellSize + xOffset,
 				y * cellSize + yOffset,
 				cellSize, cellSize );
 	}
 
 	/**
+	 * Draw a hollow rect on both images.
 	 *
-	 * @param x
-	 * @param y
-	 * @param color
+	 * @param x     The starting x for the rect
+	 * @param y     The starting y for the rect
+	 * @param color The color to use to draw the rect
 	 */
+	//TODO: refactor this
 	private void drawHollowRect( int x, int y, Color color ) {
-		answerKeyWriter.setColor(color);
-		answerKeyWriter.drawRect( 	x * cellSize + xOffset,
+		answerKeyWriter.setColor( color );
+		answerKeyWriter.drawRect( x * cellSize + xOffset,
 				y * cellSize + yOffset,
 				cellSize, cellSize );
 
 		blankPuzzleWriter.setColor( color );
-		blankPuzzleWriter.drawRect( 	x * cellSize + xOffset,
+		blankPuzzleWriter.drawRect( x * cellSize + xOffset,
 				y * cellSize + yOffset,
 				cellSize, cellSize );
 	}
 
 	/**
 	 * Create a Graphics2D object to work correctly with the BufferedImage.
+	 *
 	 * @param img The BufferedImage that this Graphics2D object will be writing to.
+	 *
 	 * @return The initialized Graphics2D object.
 	 */
 	private Graphics2D createGraphics2dWriter( BufferedImage img ) {
-		Graphics2D g2d = img.createGraphics();
+		Graphics2D g2d = img.createGraphics( );
 		g2d.setColor( Color.white );
 		g2d.fillRect( 0, 0, IMAGE_WIDTH, IMAGE_HEIGHT );
 		g2d.setFont( new Font( "TimesRoman", Font.PLAIN, fontSize ) );
